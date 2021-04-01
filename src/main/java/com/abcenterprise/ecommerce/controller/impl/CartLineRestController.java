24ba60ae7d89bfa17adaa275329b6128a3b6e154
EtiennePS <etienne.picherit@gmail.com>
2021-04-01 15:18:22 +0200

    Ajout d'une route pour accéder aux lignes de panier du user connecté

--- src/main/java/com/abcenterprise/ecommerce/controller/impl/CartLineRestController.java
+++ src/main/java/com/abcenterprise/ecommerce/controller/impl/CartLineRestController.java
@@ -3,11 +3,13 @@ package com.abcenterprise.ecommerce.controller.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.abcenterprise.ecommerce.config.SecurityConfig;
 import com.abcenterprise.ecommerce.model.dto.CartLineDto;
 import com.abcenterprise.ecommerce.model.mapper.BusinessMapper;
 import com.abcenterprise.ecommerce.service.ICartLineService;
@@ -26,6 +28,7 @@ public class CartLineRestController {
 	private BusinessMapper mapper;
 	
 	@GetMapping("/")
+	@PreAuthorize(SecurityConfig.CAN_SEE_ALL_ORDERS)
 	@ApiOperation(authorizations = @Authorization(value = "Bearer"), value = "")
 	public CollectionModel<CartLineDto> getAll() {
 		return mapper.toCartLineDto(cartLineService.getAll());
@@ -35,5 +38,11 @@ public class CartLineRestController {
 	@ApiOperation(authorizations = @Authorization(value = "Bearer"), value = "")
 	public CartLineDto getById(@PathVariable Long id) {
 		return mapper.toDto(cartLineService.getById(id));
+	}
+	
+	@GetMapping("/mines")
+	@ApiOperation(authorizations = @Authorization(value = "Bearer"), value = "")
+	public CollectionModel<CartLineDto> getAllFromUser() {
+		return mapper.toCartLineDto(cartLineService.getAllByConnectedUser());
 	}
 }
